a4f4bd45abec05f7cf149b334c141765253cc23e
Asijit Paul <asijit1610@gmail.com>
2021-01-20 17:22:28 +0530

    initial commit php with test cases

---
+++ php/test/test_location.php
@@ -0,0 +1,48 @@
+<?php
+$locdata = array(
+    0 => array(
+        "from" => "Dallas,TX",
+        "to" =>  'New York,NY'
+    ),
+    1 => Array ( 
+        'from' => 'OklahomaCity,OK-73142',
+        'to' => 'Joplin,MO-64804'
+    ),
+    2 => Array ( 
+        'from' => '1615-Dundas-St-W,Whitby,ON-L1P-1Y9,Canada',
+        'to' => '3730-Concession-Rd-8,Orono,ON-L0B-1M0,Canada'
+    ),
+    3 => Array(
+        'from' => 'Coleshill,Birmingham,UK',
+        'to' => 'Stafford,UK'
+    ),
+    4 => array(
+        "from" => "66480 Maureillas-las-Illas,France",
+        "to" =>  '37100 Tours,France'
+    ),
+    5 => array(
+        "from" => "New Springfield,OH 4443",
+        "to" =>  'Middletown,PA 17057'
+    ),
+    6 => array(
+        "from" => "69125 Colombier-Saugnieu,France",
+        "to" =>  '66480 Maureillas-las-Illas,France'
+    ),
+    7 => array(
+        "from" => "Mumbai, Maharashtra, India",
+        "to" =>  'Pune, Maharashtra, India'
+    ),
+    8 => array(
+        "from" => "2050 Jessheim,Norway",
+        "to" =>  '2670 Otta,Norway'
+    ),
+    9 => array(
+        "from" => "Caraun-More,Ireland",
+        "to" =>  'Johnstown-Way-Enfield-A83,Ireland'
+    ),
+    10 => array(
+        "from" => "Stougjesdijk 294b 3261 KW Oud Beijerland,Netherlands",
+        "to" =>  'Albert Schweitzerplaats 25 3318 AT Dordrecht,Netherlands'
+    ),
+);
+?>
