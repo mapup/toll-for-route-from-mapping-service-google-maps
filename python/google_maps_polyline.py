18588c67ef42de7d6827eaa7ac176d1459e2dd70
pavel-mapup <77106021+pavelchowdhury03@users.noreply.github.com>
2021-09-15 16:52:54 +0530

    Restructuring the program.

--- python/google_maps_polyline.py
+++ python/google_maps_polyline.py
@@ -1,23 +1,45 @@
 #Importing modules
-import json
+import os
 import requests
-import os 
+import json
 import polyline as poly
 
+# Fetching API keys for google maps and TollGuru
 #API key for google map
-token=os.environ.get('GOOGLE_API')
+GOOGLE_MAPS_API_KEY = lambda  : os.environ.get('GOOGLE_MAPS_API_KEY')
 #API key for Tollguru
-Tolls_Key = os.environ.get('Tollguru_API_New')
+TOLLGURU_API_KEY = lambda : os.environ.get('TOLLGURU_API_KEY')
 
 
-'''Fetching Polyline from Google'''
-def get_polyline_from_google_maps(source,destination):
+def get_polyline_from_google_maps(origin,destination,google_maps_api_key):
+    """
+    Fetching Polyline from Google for an origin and destination pair.
+    Parameters
+    ----------
+    source : str
+            Origin address
+    destination : str
+       Destination address
+    google_maps_api_key : str
+        Google map api key
+
+    Returns
+    -------
+    str - Google Polyline
+
+    """
+    # origin = "New York, NY"
+    # destination = "Dallas, Texas"
+    
     #Query Google maps with Key and Source-Destination coordinates
-    url = 'https://maps.googleapis.com/maps/api/directions/json?origin={a}&destination={b}&key={c}'.format(a=source,b=destination,c=token)
+    URL = f'https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={GOOGLE_MAPS_API_KEY()}'
+    
     #converting the response to json
-    response=requests.get(url).json()
+    response=requests.get(URL).json()
+    
     #extracting segments of the routes
     segments=response['routes'][0]['legs'][0]['steps']
+    
     #temp_list to store all coordinates
     coordinate_list=[]
     for i in segments:
@@ -25,49 +47,51 @@ def get_polyline_from_google_maps(source,destination):
     polyline_from_google=poly.encode(coordinate_list)
     return(polyline_from_google)
 
-        
-        
-'''Calling Tollguru API'''
+
 def get_rates_from_tollguru(polyline):
     #Tollguru querry url
-    Tolls_URL = 'https://dev.tollguru.com/v1/calc/route'
+    TollGuru_API_URL = 'https://dev.TollGuru.com/v1/calc/route'
+    
+    # polyline = get_polyline_from_google_maps(origin="New York, NY", destination='Dallas, TX',
+    #                                           google_maps_api_key=GOOGLE_MAPS_API_KEY())
+    
     #Tollguru resquest parameters
     headers = {
                 'Content-type': 'application/json',
-                'x-api-key': Tolls_Key
+                'x-api-key': TOLLGURU_API_KEY()
                 }
     params = {
-                #Explore https://tollguru.com/developers/docs/ to get best of all the parameter that tollguru has to offer 
+                #Explore https://tollguru.com/toll-api-docs to get best of all the parameter that tollguru has to offer 
                 'source': "google",
                 'polyline': polyline,                      # this is the encoded polyline that we made     
                 'vehicleType': '2AxlesAuto',                #'''Visit https://tollguru.com/developers/docs/#vehicle-types to know more options'''
-                'departure_time' : "2021-01-05T09:46:08Z"   #'''Visit https://en.wikipedia.org/wiki/Unix_time to know the time format'''
+                'departure_time' : "2021-09-16T09:46:08Z"   #'''Visit https://en.wikipedia.org/wiki/Unix_time to know the time format'''
                 }
     #Requesting Tollguru with parameters
-    response_tollguru= requests.post(Tolls_URL, json=params, headers=headers,timeout=200).json()
+    response_tollguru= requests.post(TollGuru_API_URL,
+                                     json=params, headers=headers, timeout=200).json()
+    
     #print(response_tollguru)
     #checking for errors or printing rates
     if str(response_tollguru).find('message')==-1:
         return(response_tollguru['route']['costs'])
     else:
         raise Exception(response_tollguru['message'])
+
+
+if __name__ == '__main__':
+    #Step 1 :Provide Source and Destination
+    origin = '658 Sanford Rd, Wells, ME 04090'              
+    destination = '510 Alfred Rd, Biddeford, ME 04005'
     
-
-'''Program Starts'''
-#Step 1 :Provide Source and Destination
-source = '1100 E Spring Creek Pkwy, Plano, TX 75074, United States'              
-destination = '901 S Kobayashi Rd S, Webster, TX 77598, United States'
-
-#Step 2 : Get Polyline from Google
-polyline_from_google=get_polyline_from_google_maps(source, destination)
-
-#Step 3 : Get rates from Tollguru
-rates_from_tollguru=get_rates_from_tollguru(polyline_from_google)
-
-#Print the rates of all the available modes of payment
-if rates_from_tollguru=={}:
-    print("The route doesn't have tolls")
-else:
-    print(f"The rates are \n {rates_from_tollguru}")
-
-'''Program Ends'''
+    #Step 2 : Get Polyline from Google
+    polyline_from_google=get_polyline_from_google_maps(origin, destination,GOOGLE_MAPS_API_KEY())
+    
+    #Step 3 : Get rates from Tollguru
+    rates_from_tollguru=get_rates_from_tollguru(polyline_from_google)
+    
+    #Print the rates of all the available modes of payment
+    if rates_from_tollguru=={}:
+        print("The route doesn't have tolls")
+    else:
+        print(f"The rates are \n {rates_from_tollguru}")
